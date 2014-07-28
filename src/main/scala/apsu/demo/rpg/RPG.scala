1f1bcabc796b78bb3375d500fd31e82022decb2f
David Moles <chronodm@gmail.com>
2014-07-27 20:59:55 -0700

    Initial commit

---
+++ src/main/scala/apsu/demo/rpg/RPG.scala
@@ -0,0 +1,12 @@
+package apsu.demo.rpg
+
+/**
+ * RPG
+ *
+ * @author david
+ */
+object RPG {
+  def main(args: Array[String]) {
+
+  }
+}
---
+++ src/main/scala/apsu/demo/rpg/components/Abilities.scala
@@ -0,0 +1,10 @@
+package apsu.demo.rpg.components
+
+/**
+ * Abilities
+ *
+ * @author david
+ */
+case class Abilities(str: Int) {
+
+}
